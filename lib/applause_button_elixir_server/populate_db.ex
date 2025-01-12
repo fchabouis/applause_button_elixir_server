a0b1302f707b085ce3893517c275b77043bf3d71
Francis Chabouis <fchabouis@gmail.com>
2025-01-12 17:23:42 +0100

    format file

--- lib/applause_button_elixir_server/populate_db.ex
+++ lib/applause_button_elixir_server/populate_db.ex
@@ -3,29 +3,34 @@ defmodule PopulateDB do
   alias ApplauseButtonElixirServer.Page
 
   def transform_data(%{
-    "Item" => %{
-      "claps" => %{"N" => claps},
-      "sourceIp" => %{"S" => source_ip},
-      "url" => %{"S" => url}
-    }}) do
-      %{"url" => url, "claps" => claps, "source_ip" => source_ip}
-    end
+        "Item" => %{
+          "claps" => %{"N" => claps},
+          "sourceIp" => %{"S" => source_ip},
+          "url" => %{"S" => url}
+        }
+      }) do
+    %{"url" => url, "claps" => claps, "source_ip" => source_ip}
+  end
 
-    def transform_data(%{
-    "Item" => %{
-      "claps" => %{"N" => claps},
-      "url" => %{"S" => url}
-    }}) do
-      %{"url" => url, "claps" => claps, "source_ip" => nil}
-    end
+  def transform_data(%{
+        "Item" => %{
+          "claps" => %{"N" => claps},
+          "url" => %{"S" => url}
+        }
+      }) do
+    %{"url" => url, "claps" => claps, "source_ip" => nil}
+  end
 
-    def run() do
-      req = Req.get!("https://raw.githubusercontent.com/fchabouis/applause_button_elixir_server/refs/heads/main/db/db.json")
+  def run() do
+    req =
+      Req.get!(
+        "https://raw.githubusercontent.com/fchabouis/applause_button_elixir_server/refs/heads/main/db/db.json"
+      )
 
-      req.body()
-      |> Jason.decode!()
-      |> Enum.map(fn d -> PopulateDB.transform_data(d) end)
-      |> Enum.map(fn r -> Page.changeset(%Page{}, r) end)
-      |> Enum.each(fn c -> Repo.insert(c) end)
-    end
+    req.body()
+    |> Jason.decode!()
+    |> Enum.map(fn d -> PopulateDB.transform_data(d) end)
+    |> Enum.map(fn r -> Page.changeset(%Page{}, r) end)
+    |> Enum.each(fn c -> Repo.insert(c) end)
+  end
 end
