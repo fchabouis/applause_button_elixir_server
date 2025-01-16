815299815eb2b553e8278177c3a826424fbf91e4
Francis Chabouis <fchabouis@gmail.com>
2025-01-16 14:46:56 +0100

    read page url from referer

--- lib/applause_button_elixir_server_web/controllers/page_controller.ex
+++ lib/applause_button_elixir_server_web/controllers/page_controller.ex
@@ -4,14 +4,32 @@ defmodule ApplauseButtonElixirServerWeb.PageController do
   alias ApplauseButtonElixirServer.Page
   require Logger
 
+
   def clean_url(page_url) do
     page_uri = URI.parse(page_url)
     String.replace(page_url, "#{page_uri.scheme}://", "", global: false)
   end
 
+  def add_claps(%{"query_params" => %{"url" => page_url}} = conn, _params) do
+    page_url = page_url |> clean_url()
+    Logger.info("claps creation for #{page_url} from url parameter")
+    add_claps_aux(conn, page_url)
+  end
+
   def add_claps(conn, _params) do
-    %{"url" => page_url} = conn.query_params
-    page_url = clean_url(page_url)
+    page_url = conn |> page_url_from_referer!()
+    Logger.info("claps creation for #{page_url} from referer header")
+    add_claps_aux(conn, page_url)
+  end
+
+  def page_url_from_referer!(conn) do
+    case conn |> get_req_header("referer") do
+      [page_url] -> clean_url(page_url)
+      _ -> raise Plug.BadRequestError
+    end
+  end
+
+  def add_claps_aux(conn, page_url) do
     {:ok, body, conn} = Plug.Conn.read_body(conn)
     [claps_to_add, _js_version] = body |> String.replace("\"", "") |> String.split(",")
     claps_to_add = String.to_integer(claps_to_add)
@@ -76,16 +94,30 @@ defmodule ApplauseButtonElixirServerWeb.PageController do
     claps_to_add
   end
 
-  def get_claps(conn, %{"url" => url}) do
-    url = clean_url(url)
-
-    n =
-      case Page
-           |> Repo.get_by(url: url) do
-        nil -> 0
-        %{claps: n} -> n
-      end
+  def get_claps(conn, %{"url" => page_url}) do
+    Logger.info("get claps for #{page_url} from url parameter")
+    n = page_url
+    |> clean_url()
+    |> get_claps_from_db()
 
     text(conn, n)
+  end
+
+  def get_claps(conn, _) do
+    page_url = conn
+    |> page_url_from_referer!()
+
+    Logger.info("get claps for #{page_url} from referer header")
+
+    n = page_url |> get_claps_from_db()
+    text(conn, n)
+  end
+
+  def get_claps_from_db(url) do
+    case Page
+         |> Repo.get_by(url: url) do
+      nil -> 0
+      %{claps: n} -> n
+    end
   end
 end
