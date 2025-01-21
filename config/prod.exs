7c650fe6068fd8596f3e4bb6de97cee0c4dc09e8
Francis Chabouis <fchabouis@gmail.com>
2025-01-21 11:01:03 +0100

    new try

--- config/prod.exs
+++ config/prod.exs
@@ -7,7 +7,7 @@ import Config
 # before starting your production server.
 config :applause_button_elixir_server, ApplauseButtonElixirServerWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
-  url: [host: "applause.chabouis.fr", port: 443, scheme: "https"]
+  check_origin: ["https://applause.chabouis.fr"]
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: ApplauseButtonElixirServer.Finch
